726315c47cabd54c36c158fbf3a4a4bbc2d63730
Karl Rapur <rapurkarl@gmail.com>
2021-05-05 15:55:44 +0300

    Navbar to class component

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,36 +1,45 @@
 import "./Nav.css";
+import React, { Component } from "react";
 import { InputText } from "primereact/inputtext";
-import { Button } from "primereact/button";
 import logo from "../../assets/profile.png"; // Tell webpack this JS file uses this image
+import Userpopup from "../userpopup/Userpopup";
 
-function Nav() {
-  return (
-    <div className="Nav p-shadow-10">
-      <div className="p-grid p-jc-between nested-grid vertical-container p-ai-center p-p-2">
-        <div className="p-col-2">
-          <Button label="" icon="pi pi-bars" />
-        </div>
+export default class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked1: false,
+      checked2: true,
+    };
+  }
 
-        <div className="p-col-4 p-fluid ">
-          <span className="p-input-icon-left p-field">
-            <i className="pi pi-search" />
-            <InputText placeholder="Search" />
-          </span>
-        </div>
+  render() {
+    return (
+      <div className="Nav p-shadow-10">
+        <div className="p-grid p-jc-between nested-grid vertical-container p-ai-center p-p-2">
+          <div className="p-col-2 p-jc-start">
+            <Userpopup />
+          </div>
 
-        <div className="p-col-4 p-md-2">
-          <div className="p-grid vertical-container p-jc-end p-mr-2 p-ai-center">
-            <h3>Lambi Lõdvik</h3>
-            <img
-              className="p-ml-3 profilepic"
-              src={logo}
-              alt="Italian Trulli"
-            />
+          <div className="p-col-4 p-fluid ">
+            <span className="p-input-icon-left p-field">
+              <i className="pi pi-search" />
+              <InputText placeholder="Search" />
+            </span>
+          </div>
+
+          <div className="p-col-4 p-md-2">
+            <div className="p-grid vertical-container p-jc-end p-mr-2 p-ai-center">
+              <h3>Lambi Lõdvik</h3>
+              <img
+                className="p-ml-3 profilepic"
+                src={logo}
+                alt="Italian Trulli"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Nav;
