ad330574502ed2da9d0f443aa4c63c4318b8308e
Karl Rapur <rapurkarl@gmail.com>
2021-05-05 13:27:47 +0300

    Added PrimeReact support.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 function App() {
   return (
