4c19de053deda56b98ba7a8c1769e0927e415256
Karl Rapur <rapurkarl@gmail.com>
2021-05-11 12:24:48 +0300

    P-fluid & p-field example

--- src/views/threadCreation/threadCreation.js
+++ src/views/threadCreation/threadCreation.js
@@ -1,59 +1,68 @@
 import "./threadCreation.css";
-import React, {Component} from "react";
-import {InputText} from 'primereact/inputtext';
-import {Editor} from 'primereact/editor';
-import {Button} from 'primereact/button';
-import {Chips} from 'primereact/chips';
-import {Dropdown} from 'primereact/dropdown';
-
+import React, { Component } from "react";
+import { InputText } from "primereact/inputtext";
+import { Editor } from "primereact/editor";
+import { Button } from "primereact/button";
+import { Chips } from "primereact/chips";
+import { Dropdown } from "primereact/dropdown";
 
 export default class ThreadCreation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedSubforum: null
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSubforum: null,
+    };
 
-        this.subforums = [
-            {name: 'Software development', code: 'SD'},
-            {name: 'Hardware', code: 'HD'},
-            {name: 'Market', code: 'MK'},
-            {name: 'Jobs / Career', code: 'JC'},
-        ];
+    this.subforums = [
+      { name: "Software development", code: "SD" },
+      { name: "Hardware", code: "HD" },
+      { name: "Market", code: "MK" },
+      { name: "Jobs / Career", code: "JC" },
+    ];
 
-        this.onSubforumChange = this.onSubforumChange.bind(this);
+    this.onSubforumChange = this.onSubforumChange.bind(this);
+  }
 
-    }
+  onSubforumChange(e) {
+    this.setState({ selectedSubforum: e.value });
+  }
 
-    onSubforumChange(e) {
-        this.setState({selectedSubforum: e.value});
-    }
+  render() {
+    return (
+      <div className="ThreadCreation p-grid p-nogutter p-jc-center">
+        <div className="p-col-11 p-md-8 p-fluid">
+          <h3>Subject </h3>
+          <InputText id="threadName" className="p-field" />
+        </div>
 
-    render() {
-        return (
-            <div className="ThreadCreation p-grid p-nogutter p-ml-4">
-                <div className="p-col-12">
-                    <h3>Subject </h3>
-                    <InputText id="threadName" className="p-col-11"/>
-                </div>
-                <div className="TextEditorBox p-col-11 p-mt-6">
-                    <div className="p-col-1">
-                        <h3>Text </h3>
-                    </div>
-                    <Editor style={{height: '320px'}} className="p-mb-3"/>
-                    <h3>Tags</h3>
-                    <Chips id="tags" value={this.state.values1} onChange={(e) => this.setState({values1: e.value})}
-                           className="p-mb-3" separator=","/>
+        <div className="TextEditorBox p-col-11 p-md-8 p-fluid">
+          <div className="p-col-1">
+            <h3>Text </h3>
+          </div>
+          <Editor style={{ height: "320px" }} className="p-mb-3" />
+          <h3>Tags</h3>
+          <Chips
+            id="tags"
+            value={this.state.values1}
+            onChange={e => this.setState({ values1: e.value })}
+            className="p-mb-3"
+            separator=","
+          />
 
-                    <h3>Subforum</h3>
-                    <Dropdown optionLabel="name" value={this.state.selectedSubforum} options={this.subforums}
-                              onChange={this.onSubforumChange} placeholder="Select a subforum" className="p-mb-3"/>
-                    <div>
-                        <Button label="Submit" loadingOptions={{position: 'right'}}/>
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
+          <h3>Subforum</h3>
+          <Dropdown
+            optionLabel="name"
+            value={this.state.selectedSubforum}
+            options={this.subforums}
+            onChange={this.onSubforumChange}
+            placeholder="Select a subforum"
+            className="p-mb-3"
+          />
+          <div>
+            <Button label="Submit" loadingOptions={{ position: "right" }} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
