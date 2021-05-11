cc1e1cec98cd0f04920af1ca4241e85272aeba6a
Karl Rapur <rapurkarl@gmail.com>
2021-05-11 10:44:12 +0300

    Threadheader color update

--- src/components/threadheader/threadheader.js
+++ src/components/threadheader/threadheader.js
@@ -1,5 +1,6 @@
 import "./threadheader.css";
 import React, { Component } from "react";
+import { Divider } from "primereact/divider";
 
 export default class Threadheader extends Component {
   constructor(props) {
@@ -12,12 +13,12 @@ export default class Threadheader extends Component {
 
   render() {
     return (
-      <div className="Threadheader p-grid p-nogutter">
-        <div className="p-col-8 p-shadow-2">
+      <div className="Threadheader p-col-12 p-md-8 p-grid p-nogutter">
+        <div className="p-col-8">
           <h3>{this.props.title}</h3>
           <h5>Started by {this.props.author}</h5>
         </div>
-        <div className="p-col-2 p-jc-center">
+        <div className="p-col-2">
           <h5>{this.props.replies} Replies</h5>
           <h5>{this.props.views} Views</h5>
         </div>
