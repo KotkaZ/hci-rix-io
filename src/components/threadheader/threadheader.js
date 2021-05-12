11cbab65efbf35450141c725e98d6f66122d5ca8
Karl Rapur <rapurkarl@gmail.com>
2021-05-12 13:10:34 +0300

    Threadheader alignment improvment.

--- src/components/threadheader/threadheader.js
+++ src/components/threadheader/threadheader.js
@@ -1,5 +1,6 @@
 import "./threadheader.css";
 import React, { Component } from "react";
+import { Divider } from "primereact/divider";
 
 export default class Threadheader extends Component {
   constructor(props) {
@@ -14,17 +15,18 @@ export default class Threadheader extends Component {
     return (
       <div className="Threadheader p-col-12 p-md-8 p-grid p-nogutter">
         <div className="p-col-8">
-          <h3>{this.props.title}</h3>
-          <h5>Started by {this.props.author}</h5>
+          <h3 className="p-m-2">{this.props.title}</h3>
+          <h5 className="p-m-2">Started by {this.props.author}</h5>
         </div>
         <div className="p-col-2">
-          <h5>{this.props.replies} Replies</h5>
-          <h5>{this.props.views} Views</h5>
+          <h5 className="p-m-2">{this.props.replies} Replies</h5>
+          <h5 className="p-m-2">{this.props.views} Views</h5>
         </div>
         <div className="p-col-2">
-          <h5>{this.props.lastpost}</h5>
-          <h5>by {this.props.lastposter}</h5>
+          <h5 className="p-m-2">{this.props.lastpost}</h5>
+          <h5 className="p-m-2">by {this.props.lastposter}</h5>
         </div>
+        <Divider />
       </div>
     );
   }
