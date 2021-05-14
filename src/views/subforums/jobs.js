1faf3976a4cf1ba7f9ccc04be04225ee1f452f2e
Karl Rapur <rapurkarl@gmail.com>
2021-05-15 00:43:44 +0300

    New jobs data loading

--- src/views/subforums/jobs.js
+++ src/views/subforums/jobs.js
@@ -1,45 +1,12 @@
 import React, { Component } from "react";
 import Threadgroup from "../../components/threadgroup/threadgroup";
-import ThreadGroupElement from "../../components/threadgroupelement/threadgroupelement";
+import data from "../../data/jobs.json";
 
 export default class Jobs extends Component {
   render() {
-    const softwareThreads = [
-      <ThreadGroupElement
-        title="Fullstack developer"
-        desc="Fullstack developers"
-      />,
-      <ThreadGroupElement
-        title="Frontend developer"
-        desc="Only frontend jobposts"
-      />,
-      <ThreadGroupElement title="Backend developer" desc="All backend jobs" />,
-      <ThreadGroupElement
-        title="Cloud engineer"
-        desc="Only cloud engineering jobs"
-      />,
-      <ThreadGroupElement
-        title="Software architect"
-        desc="Architects, look jobs here!"
-      />,
-      <ThreadGroupElement title="Misc" desc="All the others" />,
-    ];
-
-    const hardwareThreads = [
-      <ThreadGroupElement
-        title="Server maintainer"
-        desc="Lorem ipsum dolor sit amet"
-      />,
-      <ThreadGroupElement
-        title="Lorem ipsum dolor sit amet"
-        desc="Lorem ipsum dolor sit amet"
-      />,
-    ];
-
-    const groups = [
-      <Threadgroup title="Software developers ♥" threads={softwareThreads} />,
-      <Threadgroup title="The hardware guys" threads={hardwareThreads} />,
-    ];
+    const groups = data.map(data => {
+      return <Threadgroup title={data.title} threads={data.threads} />;
+    });
 
     return (
       <div className="p-grid p-jc-center">
