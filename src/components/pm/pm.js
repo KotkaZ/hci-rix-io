cbe7abd692bcc50b8182d36d84d337847d990590
Robert Reimann <robi.reimann@gmail.com>
2021-05-16 19:52:02 +0300

    made pm a component and added sending messages feature

--- src/components/pm/pm.js
+++ src/components/pm/pm.js
@@ -1,24 +1,37 @@
 import "./pm.css";
-import React from "react";
+import React, {Component} from "react";
 
 // <Pm text="Hello World!" time="15:15" date="12/12/12" type="{start|end}"/>
-function Pm(props) {
-    return (
-        <div className="Pm p-d-block">
-            <div className={`message ${props.type}-type p-p-3 p-grid p-justify-${props.type}`}>
-                <table>
-                    <tr>
-                        <div className={`message-box p-p-3 p-mb-0 p-shadow-5`}>
-                            <p className={`p-m-0`}> {props.text} </p>
-                        </div>
-                     </tr>
-                    <tr>
-                        <p className={`message-datetime p-m-0`}> {props.time} {props.date}</p>
-                    </tr>
-                </table>
+export default class pm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHover: 1,
+            chatPartner: {
+                imgSrc: "",
+                name: "",
+            },
+        };
+    }
+
+
+    render() {
+        return (
+            <div className="Pm p-d-block">
+                <div className={`message ${this.props.type}-type p-p-3 p-grid p-justify-${this.props.type}`}>
+                    <table>
+                        <tr>
+                            <div className={`message-box p-p-3 p-mb-0 p-shadow-5`}>
+                                <p className={`p-m-0`}> {this.props.text} </p>
+                            </div>
+                        </tr>
+                        <tr>
+                            <p className={`message-datetime p-m-0`}> {this.props.time} {this.props.date}</p>
+                        </tr>
+                    </table>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default Pm;
