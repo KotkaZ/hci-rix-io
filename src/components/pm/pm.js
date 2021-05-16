22c3ab5a01e1acfefb59b4f9006ac666310f3f42
Robert Reimann <robi.reimann@gmail.com>
2021-05-16 22:13:05 +0300

    fixed extremely long string wrapping in DMs

--- src/components/pm/pm.js
+++ src/components/pm/pm.js
@@ -19,7 +19,7 @@ export default class pm extends Component {
         return (
             <div className="Pm p-d-block">
                 <div className={`message ${this.props.type}-type p-p-3 p-grid p-justify-${this.props.type}`}>
-                    <div style={{width: '10rem', maxWidth: "35vw"}} className="message-box p-p-3 p-mb-0 p-shadow-3 p-flex-wrap">
+                    <div className="message-box p-p-3 p-mb-0 p-shadow-3 p-flex-wrap">
                         <p className="p-m-0 p-jc-center">{this.props.text}</p>
                     </div>
                     <p className="message-datetime p-col-12"> {this.props.time} {this.props.date}</p>
