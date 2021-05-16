3aa0b0304edcb29d2f47e2ede7fe17643220f60e
Karl Rapur <rapurkarl@gmail.com>
2021-05-16 17:27:55 +0300

    PM header improvments

--- src/components/prePm/prePm.js
+++ src/components/prePm/prePm.js
@@ -1,49 +1,74 @@
 import "./prePm.css";
-import React, {Component} from "react";
-import {Avatar} from 'primereact/avatar';
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Avatar } from "primereact/avatar";
+import { Link } from "react-router-dom";
+import { Button } from "primereact/button";
 
 // <PrePm name="John Doe" imgSrc="...."/>
 export default class PrePm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isHover: 1,
-            chatPartner: {
-                imgSrc: "",
-                name: "",
-            },
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHover: 1,
+      chatPartner: {
+        imgSrc: "",
+        name: "",
+      },
+    };
+  }
 
-    goToChat = () => {
-        console.log("klicked")
-    }
+  goToChat = () => {
+    console.log("klicked");
+  };
 
-    hoverStart = () => {
-        this.setState({isHover: 5});
-    }
+  hoverStart = () => {
+    this.setState({ isHover: 5 });
+  };
 
-    hoverEnd = () => {
-        this.setState({isHover: 1});
-    }
+  hoverEnd = () => {
+    this.setState({ isHover: 1 });
+  };
 
-    render() {
-
-        return (
-            <div className="PrePm p-d-block">
-                <Link className="link" to="/Unga_Punga">
-                    <div className={`p-d-block p-p-2 p-d-flex p-ai-center p-shadow-${this.state.isHover}`}
-                         style={{fontSize: 30, verticalAlign: 'center',}}
-                         onMouseEnter={this.hoverStart}
-                         onMouseLeave={this.hoverEnd}
-                         onClick={this.goToChat}
-                    >
-                        <Avatar className="p-m-2" image={this.props.imgSrc} shape="circle" size="large"/>
-                        {this.props.name}
-                    </div>
-                </Link>
+  render() {
+    return (
+      <div className="PrePm p-d-block">
+        <Link className="link" to="/Unga_Punga">
+          <div
+            className={`p-grid p-jc-between p-ai-center p-shadow-${this.state.isHover}`}
+            style={{ verticalAlign: "center" }}
+            onMouseEnter={this.hoverStart}
+            onMouseLeave={this.hoverEnd}
+            onClick={this.goToChat}
+          >
+            <div className="p-grid p-col-4 p-ai-center p-p-2 p-nogutter">
+              <Avatar
+                className="p-m-2"
+                image={this.props.imgSrc}
+                shape="circle"
+                size="large"
+              />
+              <div>
+                <h3 className="p-m-0">{this.props.name}</h3>
+                <p className="p-m-0">{this.props.date}</p>
+              </div>
             </div>
-        );
-    }
+            <div>
+              <p className="p-m-0 p-pr-3">{this.props.lastText}</p>
+            </div>
+            <div>
+              <Button
+                icon="pi pi-times"
+                className="p-button-rounded p-button-danger p-button-outlined p-mr-2"
+                tooltip="This feature is not supported in prototype!"
+                tooltipOptions={{
+                  className: "pink-tooltip",
+                  position: "bottom",
+                }}
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
 }
--- src/views/listPmsView/listPmsView.js
+++ src/views/listPmsView/listPmsView.js
@@ -1,58 +1,80 @@
 import "./listPmsView.css";
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PrePm from "../../components/prePm/prePm";
 
 export default class ListPmsView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            chatPartners: [
-                {
-                    imgSrc: "https://i.redd.it/v0caqchbtn741.jpg",
-                    name: "Unga Punga",
-                },
-                {
-                    imgSrc: "http://dose.ca/wp-content/uploads/2014/01/a98532_profile-pic_2.jpg",
-                    name: "Tugev Juhan",
-                },
-                {
-                    imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdZTtK6pHqjvMrFvkwyTP_WgXYLrSAdna_8w&usqp=CAU",
-                    name: "Lihtsalt Tanel",
-                },
-                {
-                    imgSrc: "https://d2eoq4xox7ijpx.cloudfront.net/cdn_asset/120/wp-content/uploads/2014/12/shutterstock_107540021.jpg",
-                    name: "Rõõmus Juss",
-                },
-                {
-                    imgSrc: "https://pfpmaker.com/_nuxt/img/profile-3-1.3e702c5.png",
-                    name: "Kahtlane Kalle",
-                },
-                {
-                    imgSrc: "https://i.ytimg.com/vi/QUvLZeDgU-c/hqdefault.jpg",
-                    name: "Peremees Anne",
-                },
-                {
-                    imgSrc: "https://image.shutterstock.com/image-illustration/anonymous-male-profile-picture-silhouette-260nw-149293433.jpg",
-                    name: "Anonüümne Toomas",
-                },
-            ]
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatPartners: [
+        {
+          imgSrc: "https://i.redd.it/v0caqchbtn741.jpg",
+          name: "Unga Punga",
+          date: "16/05/2021",
+          lastText: "Deal!",
+        },
+        {
+          imgSrc:
+            "http://dose.ca/wp-content/uploads/2014/01/a98532_profile-pic_2.jpg",
+          name: "Tugev Juhan",
+          date: "14/05/2021",
+          lastText: "Hello, I have GPU for sale!",
+        },
+        {
+          imgSrc:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdZTtK6pHqjvMrFvkwyTP_WgXYLrSAdna_8w&usqp=CAU",
+          name: "Lihtsalt Tanel",
+          date: "10/05/2021",
+          lastText: "I really enjoyed your guide!",
+        },
+        {
+          imgSrc:
+            "https://d2eoq4xox7ijpx.cloudfront.net/cdn_asset/120/wp-content/uploads/2014/12/shutterstock_107540021.jpg",
+          name: "Rõõmus Juss",
+          date: "9/04/2021",
+          lastText: "Lorem ipsum",
+        },
+        {
+          imgSrc: "https://pfpmaker.com/_nuxt/img/profile-3-1.3e702c5.png",
+          name: "Kahtlane Kalle",
+          date: "1/04/2021",
+          lastText: "Lorem ipsum",
+        },
+        {
+          imgSrc: "https://i.ytimg.com/vi/QUvLZeDgU-c/hqdefault.jpg",
+          name: "Peremees Anne",
+          date: "19/03/2021",
+          lastText: "Lorem ipsum",
+        },
+        {
+          imgSrc:
+            "https://image.shutterstock.com/image-illustration/anonymous-male-profile-picture-silhouette-260nw-149293433.jpg",
+          name: "Anonüümne Toomas",
+          date: "24/11/2020",
+          lastText: "Lorem ipsum",
+        },
+      ],
+    };
+  }
+
+  render() {
+    const items = [];
+
+    for (const item of this.state.chatPartners) {
+      items.push(
+        <PrePm
+          imgSrc={item.imgSrc}
+          name={item.name}
+          date={item.date}
+          lastText={item.lastText}
+        />
+      );
     }
 
-    render() {
-        const items = []
-
-        for (const item of this.state.chatPartners) {
-            items.push(<PrePm imgSrc={item.imgSrc} name={item.name}/>)
-        }
-
-        return (
-            <div className="ListPmsView p-grid p-justify-center">
-                <div className={"p-col-12 p-md-9"}>
-                    {items}
-                </div>
-            </div>
-
-        );
-    }
+    return (
+      <div className="ListPmsView p-grid p-justify-center">
+        <div className={"p-col-12 p-md-9"}>{items}</div>
+      </div>
+    );
+  }
 }
